269c6a528eefad08311704de5b09afef3de65cba
PawelMurawski <pawelt.murawski@gmail.com>
2019-11-25 03:15:25 +0100

    first commit

---
+++ src/main/java/com/tennis/TennisCenterApplication.java
@@ -0,0 +1,13 @@
+package com.tennis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tennis/TennisCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tennis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
