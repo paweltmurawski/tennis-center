dd1341c818b2b989b9546d63a1f97c18f660793a
PawelMurawski <pawelt.murawski@gmail.com>
2019-12-05 17:45:38 +0100

    add orders service

---
+++ src/main/java/com/tennis/service/UsersOrdersDbService.java
@@ -0,0 +1,31 @@
+package com.tennis.service;
+
+import com.tennis.domain.UsersOrders;
+import com.tennis.repository.UsersOrdersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsersOrdersDbService {
+    @Autowired
+    private UsersOrdersRepository usersOrdersRepository;
+
+    public List<UsersOrders> getAllUsersOrders() {
+        return usersOrdersRepository.findAll();
+    }
+
+    public Optional<UsersOrders> getUserOrder(final Long id) {
+        return usersOrdersRepository.findById(id);
+    }
+
+    public UsersOrders saveUserOrder(final UsersOrders usersOrders) {
+        return usersOrdersRepository.save(usersOrders);
+    }
+
+    public void deleteUserOrderById(final Long id) {
+        usersOrdersRepository.deleteById(id);
+    }
+}
