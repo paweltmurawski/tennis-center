850e0d2a954223fb93330e68bc1d34a3e9740b51
PawelMurawski <pawelt.murawski@gmail.com>
2019-12-05 18:19:43 +0100

    add cart repository

--- src/main/java/com/tennis/repository/CartRepository.java
+++ src/main/java/com/tennis/repository/CartRepository.java
@@ -6,10 +6,20 @@ import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @Transactional
 @Repository
 public interface CartRepository extends CrudRepository<Cart, Long> {
     @Override
     List<Cart> findAll();
+
+    @Override
+    Optional<Cart> findById(Long id);
+
+    @Override
+    Cart save(Cart cart);
+
+    @Override
+    void deleteById(Long id);
 }
