728c68013a9080bf3672faad36bb9a8f78ae945b
PawelMurawski <pawelt.murawski@gmail.com>
2019-12-05 17:36:26 +0100

    add orders repository

--- src/main/java/com/tennis/repository/UsersOrdersRepository.java
+++ src/main/java/com/tennis/repository/UsersOrdersRepository.java
@@ -6,10 +6,20 @@ import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @Transactional
 @Repository
 public interface UsersOrdersRepository extends CrudRepository<UsersOrders, Long> {
     @Override
     List<UsersOrders> findAll();
+
+    @Override
+    Optional<UsersOrders> findById(Long id);
+
+    @Override
+    UsersOrders save(UsersOrders usersOrders);
+
+    @Override
+    void deleteById(Long id);
 }
